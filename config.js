a0814cfa8d24768ef6d794605a6cca112ea8c3b5
zyt1016 <74715205+zyt1016@users.noreply.github.com>
2026-06-26 15:14:44 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SFX_SUPABASE_CONFIG = {
+  url: "https://aoqqlsoljteslihihmog.supabase.co",
+  anonKey: "sb_publishable_FYBVgRcLTJR-C_gqxKfZ9g_1eLJZRg6",
+  allowedEmails: ["2213074048@qq.com"]
+};
